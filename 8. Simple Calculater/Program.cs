f3812119366597db069536a27433293d6d6e8eb6
user <user@LAPTOP-KAN0NU4I>
2023-09-14 20:16:09 +0200

    Add project files.

---
+++ 8. Simple Calculater/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8.Simple_Calculater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int num3 = (int)Convert.ToDouble(Console.ReadLine());
+            int num4 = num1 + num2 + num3; 
+
+            Console.WriteLine(num1 + " + " + num2+ " + " + num3 + " = " + num4);
+
+            Console.ReadLine();
+        }
+    }
+}
